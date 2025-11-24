d5d7e4d9c7519646af4ab8c1a81e021ff60b475b
7homasSutter <9306853+7homasSutter@users.noreply.github.com>
2025-11-24 11:56:10 +0000

    Deploying to gh-pages from @ ZHAW-ISE/pizzhaw.github.io@66e46defaae68052e1e66fba85c53d7cc7eccb58 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ZHAW-ISE/Pizzhaw.github.io",
+    "data-repo": "ZHAW-ISE/pizzhaw.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
